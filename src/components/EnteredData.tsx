a6643ff374af27828341a484c680f40fdb8e6569
Rachel Miles <rachmiles@gmail.com>
2022-04-26 14:05:28 +0100

    Correct display of data

--- src/components/EnteredData.tsx
+++ src/components/EnteredData.tsx
@@ -8,11 +8,11 @@ const EnteredData: React.FC<EnteredDataProps> = ({ formData }) => (
   <>
     <h2>Data submitted on form:</h2>
     <div>
-      <div>{`Species name is: ${formData.speciesName}`}</div>
-      <div>{`Planet name is: ${formData.planetName}`}</div>
-      <div>{`Number of beings is: ${formData.numberOfBeings}`}</div>
-      <div>{`2 + 2 is: ${formData.twoPlusTwo}`}</div>
-      <div>{`Reason for sparing is: ${formData.reasonForSparing}`}</div>
+      <div>Species name is: {formData.speciesName}</div>
+      <div>Planet name is: {formData.planetName}</div>
+      <div>Number of beings is: {formData.numberOfBeings}</div>
+      <div>2 + 2 is: {formData.twoPlusTwo}</div>
+      <div>Reason for sparing is: {formData.reasonForSparing}</div>
     </div>
   </>
 );
