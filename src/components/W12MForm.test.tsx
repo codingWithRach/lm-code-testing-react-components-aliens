9987d88140365124b55dc6009bfce65518faf6c9
Rachel Miles <rachmiles@gmail.com>
2022-04-26 18:43:51 +0100

    Pass props to W12Form

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,12 +1,25 @@
-import { render } from '@testing-library/react';
-import W12MForm from './W12MForm';
+import { render } from "@testing-library/react";
+import { FormData } from "../data/FormData";
+import W12MForm from "./W12MForm";
 
-test('renders form element', () => {
-	// we can hold onto the object returned from render()
-	// this object has a container property that we can destructure and inspect
-	const { container } = render(<W12MForm />);
+test("renders form element", () => {
+  const dummyData: FormData = {
+    speciesName: "Hippo",
+    planetName: "Earth",
+    numberOfBeings: "564654",
+    twoPlusTwo: "4",
+    reasonForSparing: "Gorgeous creatures",
+    submitted: false,
+  };
+  const mockFunction = jest.fn();
 
-	// the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
-	// for example, the firstChild of our container should be our form element
-	expect(container.firstChild).toHaveClass('w12MForm');
+  // we can hold onto the object returned from render()
+  // this object has a container property that we can destructure and inspect
+  const { container } = render(
+    <W12MForm formData={dummyData} handleFormData={mockFunction} />
+  );
+
+  // the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
+  // for example, the firstChild of our container should be our form element
+  expect(container.firstChild).toHaveClass("w12MForm");
 });
