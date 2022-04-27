0d74b24e3f8353d0223dce6ac6c7bea2e93f63c0
Rachel Miles <rachmiles@gmail.com>
2022-04-27 08:40:51 +0100

    Add test for change handler

--- src/components/Textarea/Textarea.test.tsx
+++ src/components/Textarea/Textarea.test.tsx
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import TextArea from "./Textarea";
+
+configure({ adapter: new Adapter() });
 
 const dummyId = "linkToLabel";
 const dummyName = "dummy name";
@@ -30,4 +34,21 @@ test("checks that textarea is only rendered once", () => {
   );
   const elements = screen.getAllByRole("textbox");
   expect(elements.length).toEqual(1);
+});
+
+test("checks that function is called when text area is changed", () => {
+  const event = {
+    target: { value: "hello world" },
+  } as React.ChangeEvent<HTMLTextAreaElement>;
+  const element = shallow(
+    <TextArea
+      id={dummyId}
+      name={dummyName}
+      value={dummyValue}
+      onChangeHandler={mockChange}
+    />
+  );
+  element.find("textarea").simulate("change", event);
+  expect(mockChange.mock.calls.length).toEqual(1);
+  expect(mockChange.mock.calls[0][0].target.value).toBe("hello world");
 });
