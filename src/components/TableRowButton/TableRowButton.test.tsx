cd39971b7b5f858f37b68df7c1fbe437d4f29ee9
Rachel Miles <rachmiles@gmail.com>
2022-04-27 07:40:55 +0100

    Amend tests so not testing child component

--- src/components/TableRowButton/TableRowButton.test.tsx
+++ src/components/TableRowButton/TableRowButton.test.tsx
@@ -1,45 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import TableRowButton from "./TableRowButton";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Button from "../Button/Button";
+
+configure({ adapter: new Adapter() });
 
 const dummyButtonText = "Click here";
 const mockClick = jest.fn();
 
-test("renders element with expected text", () => {
-  render(
+test("renders Button (child component)", () => {
+  const wrapper = mount(
     <TableRowButton labelText={dummyButtonText} onClickHandler={mockClick} />
   );
-  const buttonElement = screen.getByText(dummyButtonText);
-  expect(buttonElement).toBeInTheDocument();
-});
-
-test("checks that button with this text is rendered once", () => {
-  render(
-    <TableRowButton labelText={dummyButtonText} onClickHandler={mockClick} />
-  );
-  const buttonElements = screen.getAllByText(dummyButtonText);
-  expect(buttonElements.length).toEqual(1);
-});
-
-test("checks that element is a button", () => {
-  render(
-    <TableRowButton labelText={dummyButtonText} onClickHandler={mockClick} />
-  );
-  const buttonElement = screen
-    .getAllByRole("button")
-    .find((b) => b.textContent === dummyButtonText);
-  expect(buttonElement).toHaveAccessibleName(dummyButtonText);
-});
-
-test("checks that function is called when button is clicked", () => {
-  render(
-    <TableRowButton labelText={dummyButtonText} onClickHandler={mockClick} />
-  );
-  const buttonElement = screen
-    .getAllByRole("button")
-    .find((b) => b.textContent === dummyButtonText);
-  if (buttonElement) {
-    userEvent.click(buttonElement);
-  }
-  expect(mockClick.mock.calls.length).toEqual(1);
+  const component = wrapper.find(Button);
+  expect(component.length).toBe(1);
 });
