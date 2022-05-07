ca48e2e804e11eb966f04df7524d5669d26e7de3
Rachel Miles <rachmiles@gmail.com>
2022-05-07 15:03:11 +0100

    Correct mounting of W12Form

--- src/components/W12MForm/W12MForm.test.tsx
+++ src/components/W12MForm/W12MForm.test.tsx
@@ -1,8 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { configure, mount, shallow } from "enzyme";
+import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { FormData } from "../../data/FormData";
 import TableRowSubmitButton from "../TableRowSubmitButton/TableRowSubmitButton";
 import TableRowInput from "../TableRowInput/TableRowInput";
 import TableRowSelect from "../TableRowSelect/TableRowSelect";
@@ -13,65 +12,43 @@ import SubmitButton from "../SubmitButton/SubmitButton";
 
 configure({ adapter: new Adapter() });
 
-const dummyData: FormData = {
-  speciesName: "Hippo",
-  planetName: "Earth",
-  numberOfBeings: "564654",
-  twoPlusTwo: "4",
-  reasonForSparing: "Gorgeous creatures",
-  submitted: false,
-};
-const mockFunction = jest.fn();
-
 test("renders form element", () => {
-  const { container } = render(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const { container } = render(<W12MForm />);
   expect(container.firstChild).toHaveClass("w12MForm");
 });
 
 test("renders W12MHeader (child component)", () => {
-  const wrapper = mount(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const wrapper = mount(<W12MForm />);
   const component = wrapper.find(W12MHeader);
   expect(component.length).toBe(1);
 });
 
 test("renders 3 instances of TableRowInput (child component)", () => {
-  const wrapper = mount(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const wrapper = mount(<W12MForm />);
   const component = wrapper.find(TableRowInput);
   expect(component.length).toBe(3);
 });
 
 test("renders 1 instance of TableRowSelect (child component)", () => {
-  const wrapper = mount(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const wrapper = mount(<W12MForm />);
   const component = wrapper.find(TableRowSelect);
   expect(component.length).toBe(1);
 });
 
 test("renders 1 instance of TableRowTextArea (child component)", () => {
-  const wrapper = mount(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const wrapper = mount(<W12MForm />);
   const component = wrapper.find(TableRowTextarea);
   expect(component.length).toBe(1);
 });
 
 test("renders 1 instance of TableRowSubmitButton (child component)", () => {
-  const wrapper = mount(
-    <W12MForm formData={dummyData} handleFormData={mockFunction} />
-  );
+  const wrapper = mount(<W12MForm />);
   const component = wrapper.find(TableRowSubmitButton);
   expect(component.length).toBe(1);
 });
 
 test("renders components in a table", () => {
-  render(<W12MForm formData={dummyData} handleFormData={mockFunction} />);
+  render(<W12MForm />);
   const element = screen.getByRole("table");
   expect(element).toBeInTheDocument();
 });
@@ -87,7 +64,7 @@ test("renders components in a table", () => {
 // });
 
 test("when the species name is valid, doesn't render error message", async () => {
-  render(<W12MForm formData={dummyData} handleFormData={mockFunction} />);
+  render(<W12MForm />);
   const element = screen.getByLabelText("Species Name");
   await userEvent.type(element, "valid");
   const buttonComponent = screen.getByRole("button");
